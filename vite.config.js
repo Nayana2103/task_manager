1b5cd5a6b39b8036736f6a9f8112be53d3223309
Nayana2103 <nayanmathews35@gmail.com>
2025-08-19 15:27:59 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "./", // for relative paths (important for GitHub Pages or file hosting)
   plugins: [react(), tailwindcss()],
 });
