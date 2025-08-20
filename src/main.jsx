985b8a19f35dad2e0af15d9d8bf20c594efda180
Nayana2103 <nayanmathews35@gmail.com>
2025-08-20 12:38:28 +0530

    ll

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((reg) => reg.unregister());
-  });
-}
